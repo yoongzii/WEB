c36062e7db3d591b92a3c8e14553d9a6cfab093b
Jw <yjw_0104@naver.com>
2024-10-30 16:04:55 +0900

    더보기 수정

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ if(scrollTop > 1080){
 }
 else if (scrollTop < 1080){
    $('.util ul').show()
-   // $('.util button, .util a, .nav a').css('color','#2A2E34')
    $('.util button, .util a, .nav a').css('color','#E9EAEC')
 }
 })
@@ -31,69 +30,82 @@ const x = e.clientX
 const y = e.clientY
 $('.cursor').css({left: x , top: y})
 
+})//mousemove
+
 $('button, a, .img_box img, .img_wrap img, .click_img').on('mouseover',function(){
-   $('.cursor').css({width: '80px', height:'80px',color : 'purple',background: "rgba(228,182,26,0.6)"}).text("Click")
+   $('.cursor').css({width: '80px', height:'80px',color : 'purple', background: "rgba(228,182,26,0.6)"}).text("Click")
    $(this).on('mouseleave', function(e){
    $('.cursor').css({width: '12px', height:'12px', background: "rgba(228,182,26,1)"}).text("")
    })
 })
+
+//exhib_ container
+const items = $('.container_wrap .container');
+const openBox = $('.container_wrap .moreBox .openBox');
+const total = $('.container_wrap .container .item').length;
+let height = 450;
+
+$('.container_wrap .moreBox').on('click',function(e){
+   e.preventDefault();
+   if(openBox.text() === '더보기'){
+      height += 450;
+      items.css('height', height)
+   }
+      else if(openBox.text() === '더보기'){
+         openBox.text('접기')
+   } else{
+      openBox.text('더보기')}
+
 })
+
+i = 0
+const showitem = $('.container_wrap .container .item').slice(0, 4).length
+// $('.container_wrap .moreBox .total').text( `( ${showitem} / ${total} )`)
+
+for(i = 0; i <= total; i ++) {
+   $('.container_wrap .moreBox .total').text( `( ${i} / ${i} )`)
+}
+
+
 
 // exhib_btn
-$('.img_wrap ul li').hide();
-$('.img_wrap ul li:first').show();
+// $('.img_wrap ul li').hide();
+// $('.img_wrap ul li:first').show();
 
-$('.exhib_btns button').on('click', function() {
-   let index = $(this).parent().index();
-   $('.img_wrap ul li').hide();
-   $('.img_wrap ul li').eq(index).show();
-});
+// $('.exhib_btns button').on('click', function() {
+//    let index = $(this).parent().index();
+//    $('.img_wrap ul li').hide();
+//    $('.img_wrap ul li').eq(index).show();
+// });
 
-const images = document.querySelectorAll('.img_wrap li .click_img');
-const modal = document.querySelector('#design_exhib .modal');
-const modalImg = document.querySelector('#design_exhib .modalImg');
-const closeBtn = document.querySelector('#design_exhib .modal .close');
+// const images = document.querySelectorAll('.exhib_zip .container_wrap .img_zip .click_img');
+// const modal = document.querySelector('#design_exhib .modal');
+// const modalImg = document.querySelector('#design_exhib .modalImg');
+// const closeBtn = document.querySelector('#design_exhib .modal .close');
 
-images.forEach(image => {
-   image.addEventListener('click', function() {
-      modalImg.src = this.src;
-      modal.style.display = 'block';
-      setTimeout(() => {
-            modal.style.bottom = '0';
-      }, 10);
-   });
-   });
+// images.forEach(image => {
+//    image.addEventListener('click', function() {
+//       modalImg.src = this.src;
+//       modal.style.display = 'block';
+//       setTimeout(() => {
+//             modal.style.bottom = '0';
+//       }, 10);
+//    });
+//    });
 
-   closeBtn.addEventListener('click', function() {
-      modal.style.bottom = '-100%';
-      setTimeout(() => {
-         modal.style.display = 'none';
-      }, 500);
-   });
-
-
-   modal.addEventListener('click', function(event) {
-      if (event.target === modal) {
-         closeBtn.click();
-      }
-   });
+//    closeBtn.addEventListener('click', function() {
+//       modal.style.bottom = '-100%';
+//       setTimeout(() => {
+//          modal.style.display = 'none';
+//       }, 500);
+//    });
 
 
-const codingAs = document.querySelectorAll('.li_wrap .img_box .coding_link');
-const iframeSrc = document.querySelector('.frameBox iframe');
-const frameBox = document.querySelector('.frameBox');
-const frameClose = document.querySelector('.frame_close button');
+//    modal.addEventListener('click', function(event) {
+//       if (event.target === modal) {
+//          closeBtn.click();
+//       }
+//    });
 
-
-codingAs.forEach(codingA => {
-codingA.addEventListener('click', function(){
-   iframeSrc.src = codingA.href;
-   frameBox.style.display = 'flex';
-})
-})
-
-frameClose.addEventListener('click', function(){
-frameBox.style.display = 'none';
-})
 
 
